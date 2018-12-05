b7a9008bf2b46bc2b0de8f79ab4c594b4742475e
Fredrik Larsson <fredrik.larsson@zmartagroup.com>
2018-12-05 08:55:58 +0100

    Day 5 completed

---
+++ Day 5/5.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+
+const input = fs.readFileSync('./input.txt', 'utf8').trim()
+
+// const input = 'dabAcCaCBAcCcaDA'
+
+const pairs = 'abcdefghijklmnopqrstuvwxyz'.split('').reduce((acc, letter) => {
+  acc.push(letter + letter.toUpperCase())
+  acc.push(letter.toUpperCase() + letter)
+
+  return acc
+}, [])
+
+function react (input) {
+  let oldInput
+  do {
+    oldInput = input
+    // console.log(input)
+    for (var i = 0; i < pairs.length; i++) {
+      input = input.replace(pairs[i], '')
+    }
+  } while (oldInput !== input)
+
+  return input
+}
+
+function react2 (input) {
+  let shortest = input.length
+  for (var i = 0; i < pairs.length; i++) {
+    const pair = pairs[i]
+
+    const inputWithout = input.replace(RegExp(`[${pair}]`, 'g'), '')
+
+    const reacted = react(inputWithout)
+
+    if (reacted.length < shortest) shortest = reacted.length
+  }
+
+  return shortest
+}
+
+console.log(react(input).length)
+console.log(react2(input))
