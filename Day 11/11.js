d3366847c2cccd3c18a0677550d3d974c38afcff
Fredrik Larsson <fredrik.larsson@zmartagroup.com>
2018-12-11 13:40:24 +0100

    Add commented out grid visualization

--- Day 11/11.js
+++ Day 11/11.js
@@ -1,4 +1,7 @@
 const serial = 8141
+const gridSize = 300
+
+// const fs = require('fs')
 
 const cellValue = (x, y) => {
   const rackId = x + 10
@@ -9,9 +12,16 @@ const cellValue = (x, y) => {
   return hundreth - 5
 }
 
-const grid = []
+// let string = ''
+// for (let i = 0; i < gridSize; i++) {
+//   for (let j = 0; j < gridSize; j++) {
+//     string += cellValue(i, j).toString().padStart(4, ' ')
+//   }
+//   string += '\n'
+// }
+// fs.writeFileSync('output.txt', string)
 
-const gridSize = 300
+const grid = []
 
 for (let x = 0; x < gridSize; x++) {
   for (let y = 0; y < gridSize; y++) {
@@ -26,6 +36,15 @@ for (let x = 0; x < gridSize; x++) {
 
 const getCellValue = (x, y) => x < 0 || y < 0 ? 0 : grid[x + y * gridSize] || 0
 
+// string = ''
+// for (let i = 0; i < gridSize; i++) {
+//   for (let j = 0; j < gridSize; j++) {
+//     string += getCellValue(i, j).toString().padStart(6, ' ')
+//   }
+//   string += '\n'
+// }
+// fs.writeFileSync('output2.txt', string)
+
 const getSquareValue = (x, y, squareSize = 3) => {
   return getCellValue(x + squareSize - 1, y + squareSize - 1) +
     getCellValue(x - 1, y - 1) -
@@ -36,8 +55,8 @@ const getSquareValue = (x, y, squareSize = 3) => {
 let bestTopLeftX = 0
 let bestTopLeftY = 0
 let bestValue = 0
-for (let y = 0; y < gridSize - 2; y++) {
-  for (let x = 0; x < gridSize - 2; x++) {
+for (let y = 0; y < gridSize - 3; y++) {
+  for (let x = 0; x < gridSize - 3; x++) {
     const value = getSquareValue(x, y, 3)
     if (value > bestValue) {
       bestTopLeftX = x
@@ -56,7 +75,7 @@ bestTopLeftX = 0
 bestTopLeftY = 0
 bestValue = 0
 let bestSquareSize = 1
-for (let i = 0; i < gridSize; i++) {
+for (let i = 1; i <= gridSize; i++) {
   for (let y = 0; y < gridSize - i; y++) {
     for (let x = 0; x < gridSize - i; x++) {
       const value = getSquareValue(x, y, i)
