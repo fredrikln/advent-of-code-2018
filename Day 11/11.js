0b958729293ddc7d44ba8b8ac6d6db5b9bceb4c5
Fredrik Larsson <fredrik.larsson@zmartagroup.com>
2018-12-11 13:12:32 +0100

    Reimplemented Day 11 with Summed-area table algorithm

--- Day 11/11.js
+++ Day 11/11.js
@@ -1,5 +1,4 @@
 const serial = 8141
-// const serial = 42
 
 const cellValue = (x, y) => {
   const rackId = x + 10
@@ -12,28 +11,33 @@ const cellValue = (x, y) => {
 
 const grid = []
 
-for (let x = 0; x < 300; x++) {
-  for (let y = 0; y < 300; y++) {
-    grid[x + y * 300] = cellValue(x, y)
+const gridSize = 300
+
+for (let x = 0; x < gridSize; x++) {
+  for (let y = 0; y < gridSize; y++) {
+    const current = cellValue(x, y) || 0
+    const above = grid[x + (y - 1) * gridSize] || 0
+    const left = grid[(x - 1) + y * gridSize] || 0
+    const leftabove = grid[(x - 1) + (y - 1) * gridSize] || 0
+
+    grid[x + y * gridSize] = current + above + left - leftabove
   }
 }
 
-const getCellValue = (x, y) => grid[x + y * 300]
+const getCellValue = (x, y) => x < 0 || y < 0 ? 0 : grid[x + y * gridSize] || 0
+
 const getSquareValue = (x, y, squareSize = 3) => {
-  let sum = 0
-  for (let i = 0; i < squareSize; i++) {
-    for (let j = 0; j < squareSize; j++) {
-      sum += getCellValue(x + i, y + j)
-    }
-  }
-  return sum
+  return getCellValue(x + squareSize - 1, y + squareSize - 1) +
+    getCellValue(x - 1, y - 1) -
+    getCellValue(x + squareSize - 1, y - 1) -
+  getCellValue(x - 1, y + squareSize - 1)
 }
 
 let bestTopLeftX = 0
 let bestTopLeftY = 0
 let bestValue = 0
-for (let y = 0; y < 300 - 2; y++) {
-  for (let x = 0; x < 300 - 2; x++) {
+for (let y = 0; y < gridSize - 2; y++) {
+  for (let x = 0; x < gridSize - 2; x++) {
     const value = getSquareValue(x, y, 3)
     if (value > bestValue) {
       bestTopLeftX = x
@@ -44,17 +48,17 @@ for (let y = 0; y < 300 - 2; y++) {
   }
 }
 
-console.log({
+console.log(JSON.stringify({
   bestTopLeftX, bestTopLeftY, bestValue
-})
+}, null, 2))
 
 bestTopLeftX = 0
 bestTopLeftY = 0
 bestValue = 0
 let bestSquareSize = 1
-for (let i = 0; i < 300; i++) {
-  for (let y = 0; y < 300 - i; y++) {
-    for (let x = 0; x < 300 - i; x++) {
+for (let i = 0; i < gridSize; i++) {
+  for (let y = 0; y < gridSize - i; y++) {
+    for (let x = 0; x < gridSize - i; x++) {
       const value = getSquareValue(x, y, i)
       if (value > bestValue) {
         bestTopLeftX = x
@@ -64,9 +68,8 @@ for (let i = 0; i < 300; i++) {
       }
     }
   }
-  console.log(i)
 }
 
-console.log({
+console.log(JSON.stringify({
   bestTopLeftX, bestTopLeftY, bestSquareSize, bestValue
-})
+}, null, 2))
